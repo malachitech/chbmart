9a5295051fe9484ced2bf1b60779ce4ffc2eb7b8
Malachi Ezeabuchukwu <angelmalachi5@gmail.com>
2023-11-25 17:12:19 +0100

    update

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-app.use("/test", (req, res) => {
+app.use("/", (req, res) => {
   res.send("Hello world!");
 });
 
