c8643ea473da66dd1b8269563270f2db0c6c913d
Malachi Ezeabuchukwu <angelmalachi5@gmail.com>
2023-11-25 17:24:36 +0100

    update

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['http://localhost:3000'],
+  origin: ['https://allsextoys.vercel.app'],
   credentials: true
 }));
 // app.use(express.bodyParser({limit: '50mb'}))
