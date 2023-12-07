54abd4dd0908e96ce278cc822766844e259e83ec
Malachi Ezeabuchukwu <angelmalachi5@gmail.com>
2023-12-07 10:56:11 +0100

    update

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,3 @@
-// export const server = "https://allsextoysbackend.vercel.app/api/v2";
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://allsextoysbackend.vercel.app/api/v2";
 
 
