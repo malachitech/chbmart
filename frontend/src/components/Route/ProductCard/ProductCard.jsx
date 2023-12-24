b8f8d0a37c3af3fa5b744f988247904d8a378d31
malachitech <angelmalachi5@gmail.com>
2023-12-24 11:38:24 +0100

    update

--- frontend/src/components/Route/ProductCard/ProductCard.jsx
+++ frontend/src/components/Route/ProductCard/ProductCard.jsx
@@ -86,13 +86,13 @@ const ProductCard = ({ data,isEvent }) => {
           <div className="py-2 flex items-center justify-between">
             <div className="flex">
               <h5 className={`${styles.productDiscountPrice}`}>
-                {data.originalPrice === 0
+                ₦{data.originalPrice === 0
                   ? data.originalPrice
                   : data.discountPrice}
-                $
+                
               </h5>
               <h4 className={`${styles.price}`}>
-                {data.originalPrice ? data.originalPrice + " $" : null}
+                {data.originalPrice ? data.originalPrice + " ₦" : null}
               </h4>
             </div>
             <span className="font-[400] text-[17px] text-[#68d284]">
