fee4c8cde0dc162f8ee9691d13c070b64fa76237
malachitech <angelmalachi5@gmail.com>
2023-12-25 18:43:06 +0100

    update

--- frontend/src/pages/OrderSuccessPage.jsx
+++ frontend/src/pages/OrderSuccessPage.jsx
@@ -26,7 +26,7 @@ const Success = () => {
   return (
     <div>
       <Lottie options={defaultOptions} width={300} height={300} />
-      <h5 className="text-center mb-14 text-[25px] text-[#000000a1]">
+      <h5 className="text-center mb-14 text-[25px] text-[#fff]">
         Your order is successful 😍
       </h5>
       <br />
